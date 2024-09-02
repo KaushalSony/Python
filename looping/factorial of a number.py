e04341d4de21726add19e1830e02cfc5b0f445d7
Kaushal Soni <91665722+KaushalSony@users.noreply.github.com>
2024-09-02 19:34:20 +0530

    Delete looping directory

--- looping/factorial of a number.py
+++
@@ -1,5 +0,0 @@
-fact=1
-n=int(input ('Enter a Number:'))
-for i in range (n,fact,-1):
-    fact *= i
-print ("Factorial of {0:5d} is:{1:5d}".format(n,fact))
--- looping/sum and average.py
+++
@@ -1,8 +0,0 @@
-sum=0
-avg=0
-for i in range (1,11):
-    n=int(input ('Enter a Number:'))
-    sum += n
-avg=sum/10
-print ("Sum of the Numbers are:{0:5d}".format(sum))
-print ("Average of the Numbers are:{0:5.2f}".format(avg))
